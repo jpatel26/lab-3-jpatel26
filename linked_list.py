ab50cca6d4194e58eba7ab6d14d23931fa6f3686
JasminePatel <jpatel26@calpoly.edu>
2017-04-19 20:14:04 -0700

    added skeleton

--- linked_list.py
+++ linked_list.py
@@ -1,1 +1,41 @@
-#testing
+import unittest
+
+class List:
+    def __init__(self):
+
+
+# Nothing -> empty list
+def empty_list():
+
+# List integer value (any type) -> List
+# Adds an item to the list
+def add(List, index, value):
+    if index < 0 or index > #current length
+        #IndexError exception
+
+# List -> number
+# Returns the number of elements in the list
+def length(List):
+
+# List integer -> value (any type)
+# Returns the value at the given index
+def get(List, index):
+    if index < 0 or index > #current length
+        #IndexError exception
+
+# List integer value (any type) -> List
+# Replaces element at index pos with value
+def set(List, index, value):
+    if index < 0 or index > #current length
+        #IndexError exception
+
+# List integer -> Pair(element,List)
+# Removes item and returns removed item with new list
+def remove(List, index):
+    if index < 0 or index > #current length
+        #IndexError exception
+
+
+# List List-> Boolean
+# Compares List with another List
+def __eq__(self):
